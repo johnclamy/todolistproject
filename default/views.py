d9a91221757db0e1fd993b5d9d224b1d891fae42
johnclamy <johnclamy@gmail.com>
2026-03-03 11:14:57 +0000

    set the tab tile for the home index page

--- default/views.py
+++ default/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'default/index.html')
+    context = {
+        "tab_title": "Welcome to Star Buzz Coffee | Home page",
+    }
+
+    return render(request, 'default/index.html', context)
 
 
 def about(request: HttpRequest) -> HttpResponse:
