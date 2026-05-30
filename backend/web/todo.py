ed37533bb2cf826163c0646a1b2020e5f3babf9b
johnclamy <johnclamy@gmail.com>
2026-05-30 17:03:15 +0100

    set the endpoints for getting todos

--- backend/web/todo.py
+++ backend/web/todo.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from uuid import UUID
+from model.todo import Todo
+from crud.todo import read_todos, read_todo, create_todo, update_todo, delete_todo
 
 
 router = APIRouter(prefix="/api/v1/todos", tags=["todos"])
@@ -6,6 +9,14 @@ router = APIRouter(prefix="/api/v1/todos", tags=["todos"])
 
 # Set the root endpoint for the todos api
 @router.get("/")
-async def get_todos():
-    return {"todo_list": "List of todos"}
+async def get_todos() -> dict[str, list[Todo]]:
+    todos = await read_todos()
+    return {"todo_list": todos}
+
+
+# Set the endpoint for getting a single todo item by id
+@router.get("/{todo_id}")
+async def get_todo(todo_id: UUID) -> dict[str, Todo]:
+    todo = await read_todo(todo_id)
+    return {"todo": todo}
 
