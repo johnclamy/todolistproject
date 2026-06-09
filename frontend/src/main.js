a9841b82e594e9483069344c232844565315a5d2
johnclamy <johnclamy@gmail.com>
2026-06-09 14:14:16 +0100

    set a filter for todos

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,5 +9,40 @@ Alpine.plugin(persist)
 Alpine.plugin(focus)
 
 
-// API Configuration
+// API Configuration 
 // const API_BASE_URL = 'http://localhost:8000/api/v1/todos/'
+
+
+// Todo list filter
+const todoFilter = ['all', 'active', 'completed']  
+
+
+// Store-based approach (recommended for shared state across components)
+document.addEventListener('alpine:init', () => {
+    Alpine.store('todoStore', {
+        // State
+        todos: [
+            {
+                id: "c32d8b45-92fe-44f6-8b61-42c2107dfe87",
+                task: "Go to the movies",
+                is_completed: false,
+                created_at: "2026-05-01T10:00:00Z",
+            }
+        ],
+
+        is_loading: false,
+        error: null,
+        filter: todoFilter[0],
+
+        // Filter todos based on the current filter
+        get filteredTodos() {
+            if (this.filter === 'active') {
+                return this.todos.filter(todo => !todo.is_completed)
+            } else if (this.filter === 'completed') {
+                return this.todos.filter(todo => todo.is_completed)
+            }
+            return this.todos
+        },
+
+    })
+})
