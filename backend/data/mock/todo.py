8aaf17a8d2158896cc2f14ac6ba9124673c294c5
johnclamy <johnclamy@gmail.com>
2026-06-15 22:21:49 +0100

    uncomment the list of todos on the serverside

--- backend/data/mock/todo.py
+++ backend/data/mock/todo.py
@@ -6,22 +6,22 @@ from uuid import uuid4
 # with actual database calls. See data directory.
 
 mock_todos: list[Todo] = [
-    # Todo(
-    #     id=uuid4(),
-    #     task="Buy groceries",
-    #     is_completed=False,
-    #     created_at="2026-05-01T10:00:00Z"
-    # ),
-    # Todo(
-    #     id=uuid4(),
-    #     task="Walk the dog",
-    #     is_completed=True,
-    #     created_at="2026-06-01T10:00:00Z"
-    # ),
-    # Todo(
-    #     id=uuid4(),
-    #     task="Read a book",
-    #     is_completed=False,
-    #     created_at="2026-06-03T15:45:00Z"
-    # ),
+    Todo(
+        id=uuid4(),
+        task="Buy groceries",
+        is_completed=False,
+        created_at="2026-05-01T10:00:00Z"
+    ),
+    Todo(
+        id=uuid4(),
+        task="Walk the dog",
+        is_completed=True,
+        created_at="2026-06-01T10:00:00Z"
+    ),
+    Todo(
+        id=uuid4(),
+        task="Read a book",
+        is_completed=False,
+        created_at="2026-06-03T15:45:00Z"
+    ),
 ]
